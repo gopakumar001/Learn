3306a071cf4a12537b96871abf8471f4019813f2
gopakumar <gopaonline@gmail.com>
2017-04-26 20:32:06 +0530

    adding typscript skeleton code

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+/**
+ * MyClass
+ */
+class MyClass {
+
+    private name: string;
+    private desc : string;
+
+    constructor(name: string, desc: string) {
+        this.name = name;
+        this.desc = desc;
+    }
+}
